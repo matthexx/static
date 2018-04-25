88e0adf5e61f91dec46f3964482ebde2d071c635
MATTHEW <matthew_imadiyi@yahoo.com>
2018-04-25 14:37:01 +0100

    live!

---
+++ test2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8">
+        
+        <meta name="author" content="matthexx">
+
+        <title>Figma test</title>
+        
+        <link  rel="stylesheet" href="test.css" type="text/css"/>
+        
+    </head>
+    
+    <body>
+    <header>
+    
+    <div class="navArea">
+      
+        <img class="logo" src="star.png" alt="Smiley face" height="42" width="42" >
+  
+        <div class="button">
+            <a href="#"> Get started </a>
+        </div>
+        
+        </div>
+        
+    </header>
+        
+       
+     
+
+           
+      <div class="mainAres"> 
+       
+         <div class="caption">
+            
+             <h1>A great world of possibilities</h1>
+             
+             <div class="time">
+             <?php echo date ('H:i:s'); ?>
+             </div>
+         
+          </div> 
+      
+        </div>  
+        
+      
+
+    </body>
+</html>
